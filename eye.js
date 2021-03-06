d4f443b9c66bb8060ac3390b17d008163d67ee1b
MaruhknenkoDaniel <78545670+MaruhknenkoDaniel@users.noreply.github.com>
2021-03-06 12:36:21 +0200

    Update eye.js

--- eye.js
+++ eye.js
@@ -1,3 +1,8 @@
 $('.header .top .navLink').click(function() {
   $('.nav').slideToggle();
 });
+$("select").change(function() {
+  var text = $(this).find("option[value='" + $(this).val() + "']").attr("text");
+  if (text)
+    alert(text)
+})
